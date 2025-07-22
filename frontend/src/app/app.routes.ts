607f6693e6ec26128fd3e62cfe7a1e0f084d9d31
Siddhant JAdhav <SiddhantMotiram.Jadhav@cognizant.com>
2025-07-22 12:42:56 +0530

    cart showcase

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { ProductListComponent } from './modules/products/product-list/product-list.component';
 import { ProductDetailComponent } from './modules/products/product-detail/product-detail.component';
 import { AdminDashboard } from './modules/admin/admin-dashboard/admin-dashboard';
+import { CartPageComponent } from './modules/cart/pages/cart-page/cart-page.component';
 //import { HttpClientModule } from '@angular/common/http';
 
 export const routes: Routes = [
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'products/:id', component: ProductDetailComponent },
   { path: 'admin', component: AdminDashboard },
+  { path: 'cart',component:CartPageComponent},
 ];
