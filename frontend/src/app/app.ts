39d3603fae23908362b78a1d1d9369097c6312f4
KnightsWatch <khandareaniket93@gmail.com>
2025-07-27 14:58:26 +0530

    completed all the shared components , fr

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { Carousal } from './shared/components/carousal/carousal';
+import { NavBar } from './shared/components/nav-bar/nav-bar';
+import { FeatureProductList } from './shared/components/feature-product-list/feature-product-list';
+import { Footer } from './shared/components/footer/footer';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,RouterModule,Carousal],
+  imports: [RouterOutlet, RouterModule, Carousal, NavBar , FeatureProductList , Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
