114b1939981271c3365ed8a2e3affcc882b3f8f4
KnightsWatch <khandareaniket93@gmail.com>
2025-08-11 04:56:13 +0530

    integrated order history api

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {orderRouter} from "./routers/ordersRoute.js";
+import { OrderHRoute } from "./routers/orderHistoryRouter.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", orderRouter);
+app.use('/' , OrderHRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
