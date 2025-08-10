2d5c623d9fc3cceb3d3811c459b235ae240d5fcc
KnightsWatch <khandareaniket93@gmail.com>
2025-08-11 01:25:24 +0530

    track order api added

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -8,9 +8,9 @@ const url = process.env.URL;
 export const dbConnection = async () => {
     try {
         await mongoose.connect(url);
-        console.log('✅ Database connected successfully');
+        console.log(' Database connected successfully');
     } catch (error) {
-        console.error('❌ Database connection failed:', error);
+        console.error('Database connection failed:', error);
         process.exit(1); // Exit if DB fails
     }
 };
