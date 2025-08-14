b7797638816a265e9e574d2eef30c0f36085ee34
Kurakula Venu Oohitha <kurakulavenu.oohitha@cognizant.com>
2025-08-14 17:16:19 +0530

    update the page margins of all components

--- frontend/src/app/modules/products/review/review.component.ts
+++ frontend/src/app/modules/products/review/review.component.ts
@@ -6,11 +6,18 @@ import { Review } from '../../../core/services/product.service';
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ CommonModule ],
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css']
 })
 
 export class ReviewComponent {
   @Input() reviews: Review[] = [];
+
+  // Helper to generate an array for full and empty stars
+  getStarsArray(rating: number): { full: boolean }[] {
+    return Array.from({ length: 5 }, (_, index) => ({
+      full: index < rating
+    }));
+  }
 }
